f7d61369be262d84f7b0d4831cd48754945289d0
Stanovikhin Ivan <istanovikhin@croc.ru>
2021-12-14 07:56:00 +0300

    Change security config

--- src/main/java/com/example/cargoTransportation/security/SecurityConfig.java
+++ src/main/java/com/example/cargoTransportation/security/SecurityConfig.java
@@ -16,7 +16,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebM
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080")
+                .allowedOrigins("*")
                 .allowedMethods("*");
     }
 
